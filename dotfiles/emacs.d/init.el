769065e676d6c4f3a56fb9bfffd3ce295cbb3d80
Seth Lakowske <lakowske@gmail.com>
2014-04-12 11:05:06 -0500

    added many useful snippets for javascript editing

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -125,7 +125,7 @@
 (require 'yasnippet)
 (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 (yas-global-mode t)
-
+(setq yas-indent-line 'fixed)
 (define-key yas-minor-mode-map (kbd "<tab>") nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
