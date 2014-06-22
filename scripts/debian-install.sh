7de0e5626fb6719207359f316dbca87d83f6251d
Seth Lakowske <seth@epictetus.sethlakowske.com>
2014-06-22 08:29:17 -0500

    modified to work on debian and ubuntu

--- scripts/debian-install.sh
+++ scripts/debian-install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-sudo apt-get install screen emacs sudo ipython python-mode pylint python-ropemacs git-core uuid
-#su -c "apt-get install screen emacs sudo ipython python-mode pylint python-ropemacs git-core uuid"
+REQUIREMENTS="apt-get -y install screen emacs sudo ipython python-mode pylint python-ropemacs git-core uuid"
 
+install() 
+{
+    if hash sudo 2>/dev/null; then
+	echo "executing sudo install"
+	sudo $REQUIREMENTS
+    else
+	echo "executing su install"
+	su -c "$REQUIREMENTS"
+    fi
+}
+
+install
