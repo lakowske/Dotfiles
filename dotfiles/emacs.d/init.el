46a288e5657c7196596c6da0de6a25f182facbca
Seth Lakowske <lakowske@gmail.com>
2015-06-12 07:32:02 -0500

    remove unneeded dependencies.  Use Cask to track dependencies

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -1,5 +1,10 @@
 ;; -*- mode: emacs-lisp -*-
 ;; Simple .emacs configuration
+;; -------------------------------
+;; -- Ensure packages installed --
+;; -------------------------------
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
 
 ;; ---------------------
 ;; -- Global Settings --
@@ -58,20 +63,10 @@
 ;; ------------
 ;; -- Macros --
 ;; ------------
-(load "defuns-config.el")
+;; (load "defuns-config.el")
 (fset 'align-equals "\C-[xalign-regex\C-m=\C-m")
 (global-set-key "\M-=" 'align-equals)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
-(global-set-key "\C-c;" 'comment-or-uncomment-region)
-(global-set-key "\M-n" 'next5)
-(global-set-key "\M-p" 'prev5)
-(global-set-key "\M-o" 'other-window)
-(global-set-key "\M-i" 'back-window)
-(global-set-key "\C-z" 'zap-to-char)
-(global-set-key "\C-h" 'backward-delete-char)
-(global-set-key "\M-d" 'delete-word)
-(global-set-key "\M-h" 'backward-delete-word)
-(global-set-key "\M-u" 'zap-to-char)
 (global-set-key "\C-x\C-d" 'dired)
 (global-set-key "\C-x\C-r" 'query-replace)
 (global-set-key "\C-x\C-i" 'indent-region)
@@ -85,45 +80,12 @@
 (global-set-key [end] 'end-of-buffer)
 (global-set-key "\M-n" 'end-of-buffer)
 
-;; -------------------------------
-;; -- Ensure packages installed --
-;; -------------------------------
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives
-	     '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
-(package-initialize)
-
-(defvar my-packages '(better-defaults
-		      projectile
-		      clojure-mode
-		      cider))
-
-(dolist (p my-packages)
-  (unless (package-installed-p p)
-        (package-install p)))
-
-(add-to-list 'load-path "~/.emacs.d/jade-mode") ;; github.com/brianc/jade-mode
 (add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-0.8.0")
-(add-to-list 'load-path "~/.emacs.d/elpa/flymake-jshint-20140319.1500/")
-(add-to-list 'load-path "~/.emacs.d/elpa/flymake-easy-20140818.55/")
 
 ;; ---------------------------
 ;; -- JS Mode configuration --
 ;; ---------------------------
-;;(load "js-config.el")
-(require 'sws-mode)
-(require 'jade-mode)    
-(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
-(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 (add-to-list 'auto-mode-alist (cons (rx ".js" eos) 'js2-mode))
-
-;; ----------------------------
-;; -- ARC Mode configuration --
-;; ----------------------------
-;; load arc.el and auto run arc-mode when file is of .arc extension 
-(load-file "~/.emacs.d/lisp/arc.el") 
-(add-to-list 'auto-mode-alist '("\\.arc$" . arc-mode))  
 
 ;; ----------------------------
 ;; -- Original configuration --
@@ -132,17 +94,13 @@
 (setq user-email-address "lakowske@gmail.com")
 
 ;; backups are not necessary, CVS gets the job done, when the file is
-;; important, the backup file is unwanted because it poses as a
-;; security risk or just clutters up my filesystem.
+;; important, the backup file is unwanted because it poses a
+;; security risk or just clutters up the filesystem.
 (setq make-backup-files nil)
 (setq backup-inhibited t)
 (setq create-lockfiles nil)
 (setq auto-save-default nil)
-(setq tab-width 4) 
-
-
-;;(global-set-key "\C-h" 'delete-backward-char)
-;;(global-set-key "C-#" 'goto-line)
+(setq tab-width 4)
 
 ;; turn column-number-mode on
 (column-number-mode nil)
@@ -150,9 +108,6 @@
 ;; Get rid of the GUI-type stuff
 ;;(scroll-bar-mode -1)
 (menu-bar-mode -1)
-
-
-
 
 ;; Turn on snippets
 (require 'yasnippet)
@@ -170,54 +125,20 @@
 (ac-set-trigger-key "TAB")
 (ac-set-trigger-key "<tab>")
 
-
-
-
-(require 'flymake-easy)
-(require 'flymake-jshint)
-
 ;; Whitespace configuration
 (require 'whitespace)
 (global-set-key "\C-c_w" 'whitespace-mode)
 (setq whitespace-style '(face empty tabs lines-tail trailing))
 (setq whitespace-line-column 120)
 
-(add-hook 'js-mode-hook 'whitespace-mode)
-(add-hook 'js-mode-hook (lambda () (flymake-mode t)))
-(add-hook 'js-mode-hook (lambda () (setq indent-tabs-mode nil)))
-(add-hook 'js-mode-hook (lambda () (global-linum-mode t)))
-(add-hook 'js-mode-hook 'flymake-mode)
-
 (add-hook 'sh-mode-hook 'whitespace-mode)
 (add-hook 'sh-mode-hook (lambda () (global-linum-mode t)))
 (add-to-list 'load-path "~/emacs/minor-modes")
-;; Nice Flymake minibuffer messages
-(require 'flymake-cursor)
-
-;; Turns on flymake for all files which have a flymake mode
-;;(add-hook 'find-file-hook 'flymake-find-file-hook)
-
-
-
-;;(autoload 'js2-mode "js2" nil t)
-;;(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
 (add-hook 'js2-mode-hook 'skewer-mode)
 (add-hook 'css-mode-hook 'skewer-css-mode)
 (add-hook 'html-mode-hook 'skewer-html-mode)
 
-(add-to-list 'load-path "~/.emacs.d/lisp/js-comint.el")
-(require 'js-comint)
-(setq inferior-js-program-command "node")
-(setq inferior-js-mode-hook
-      (lambda ()
-        ;; We like nice colors
-        (ansi-color-for-comint-mode-on)
-        ;; Deal with some prompt nonsense
-        (add-to-list 'comint-preoutput-filter-functions
-                     (lambda (output)
-                       (replace-regexp-in-string ".*1G\.\.\..*5G" "..."
-                     (replace-regexp-in-string ".*1G.*3G" "&gt;" output))))))
 (global-set-key "\C-c\C-e" 'js-send-region)
 (global-set-key "\C-c\C-b" 'js-send-buffer)
 
@@ -260,45 +181,6 @@
 ;; add our function as the c-mode-common-hook
 (add-hook 'c-mode-common-hook 'seth-c-mode-common-hook)
 
-;; UUID generator
-;; by Christopher Wellons, 2011-11-18. Editted by Xah Lee.
-;; Edited by Hideki Saito further to generate all valid variants for "N" in xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx format.
-(defun insert-uuid-internal ()
-  "Insert a UUID. This uses a simple hashing of variable data."
-  (interactive)
-  (let ((myStr (md5 (format "%s%s%s%s%s%s%s%s%s%s"
-                (user-uid)
-                (emacs-pid)
-                (system-name)
-                (user-full-name)
-                (current-time)
-                (emacs-uptime)
-                (garbage-collect)
-                (buffer-string)
-                (random)
-                (recent-keys)))))
-
-    (insert (format "%s-%s-4%s-%s%s-%s"
-                    (substring myStr 0 8)
-                    (substring myStr 8 12)
-                    (substring myStr 13 16)
-            (format "%x" (+ 8 (random 4)))
-                    (substring myStr 17 20)
-                    (substring myStr 20 32)))))
 
 
-(setq geiser-active-implementations '(racket))
-
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize) ;; You might already have this line
-
-
-(dired ".")
-(split-window)
-(other-window 1)
-(switch-to-buffer "*scratch*")
 
