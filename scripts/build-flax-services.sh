7da7dc45362c08998a60886c7f0726e56d3d64ef
Seth Lakowske <lakowske@gmail.com>
2016-05-14 10:21:02 -0500

    scripts/build-flax-services.sh

---
+++ scripts/build-flax-services.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# if [ $# -eq 0 ]
+# then
+#     git commit -am "auto-commit all"
+# else
+#     git commit -am "$1"
+# fi
+
+# git push
+java -jar /Users/home/eclipse/McCluster/target/McCluster-0.0.1-SNAPSHOT.jar
