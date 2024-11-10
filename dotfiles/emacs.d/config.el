1db1dd453bff4fdfe776dc6fba43c9490bf2ce61
Seth Lakowske <lakowske@gmail.com>
2024-11-09 20:46:48 -0600

    start working on emacs config

--- dotfiles/emacs.d/config.el
+++ dotfiles/emacs.d/config.el
@@ -22,10 +22,10 @@
 (setq ido-enable-flex-matching t)
 (setq ido-use-faces nil)
 
-(require 'flx-ido)
-(ido-mode 1)
-(ido-everywhere 1)
-(flx-ido-mode 1)
+;; (require 'flx-ido)
+;; (ido-mode 1)
+;; (ido-everywhere 1)
+;; (flx-ido-mode 1)
 
 ;; turn column-number-mode on
 (column-number-mode nil)
